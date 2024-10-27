6d3a2ea3802f3a3a2c6045533ead30c581242d55
aditya bhojane <bhojaneaditya7@gmail.com>
2024-10-27 18:46:01 +0530

    Add bug fixes

--- src/app/pages/Contact/page.tsx
+++ src/app/pages/Contact/page.tsx
@@ -1,11 +1,9 @@
-
-import { SignupFormDemo } from '@/app/components/form/contact';
 import React from 'react'
 
 const Contact: React.FC =()=> {
   return (
     <div className='w-full h-screen '>
-      <SignupFormDemo/>
+      <h1>Coming soon ...</h1>
     </div>
   )
 }
